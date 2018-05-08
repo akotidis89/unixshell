54a8c17c2293c45e2bcf5f8b2ccde15b31cdf8ff
Anestis <akotidis89@protonmail.com>
2018-05-08 20:51:48 +0300

    contacts.sh   * Added a menu of choices for the user to select   * Minor changes in the presentation of the program
    names.dat
      * Added another record

--- contacts.sh
+++ contacts.sh
@@ -11,32 +11,71 @@
 fname=names.dat
 
 #
-#	Now, the main program starts. It will prompt the user for six items of
-#	data.
+#	First we present the user with a menu of choices to select.
 #
-echo Welcome to Contact Manager 1.0. Please enter your data:
+echo Welcome to Contact Manager. Below are the options provided:
 echo
-echo -n "First name: "; read firstname
-echo -n "Surname: "; read surname
-echo -n "Address: "; read address
-echo -n "City: "; read city
-echo -n "State: "; read state
-echo -n "Zip: "; read zip
+echo 1. Create a record
+echo 2. View records
+echo 3. Search for records
+echo 4. Delete records that match a pattern
+echo q. Exit the program
+echo
+echo
+echo -n "Please make a selection[1-4q]: "; read choice
 
 #
-#	Now, we append this information to the text file names.dat separating
-#	the six items of data with ":"
+#	Based on the user's input we create a case statement to apply the 
+#	different options.	
 #
-echo $firstname:$surname:$address:$city:$state:$zip >> $fname
+case $choice in
+	1)
+		echo "You're about to create a new record."
+		echo Please complete the appropriate fields:
+		echo
+		echo
+		echo -n "First name: "; read firstname
+		echo -n "   Surname: "; read surname
+		echo -n "   Address: "; read address
+		echo -n "      City: "; read city
+		echo -n "     State: "; read state
+		echo -n "       Zip: "; read zip
 
-#
-#	Before exiting, the contents and the count of the number of records in
-#	the file will be displayed using the "less" page program.
-#
-echo
-(echo Thank you for your input. 
-echo The contents and the numbers of records in the file will now be desplayed.
-echo
-echo Number of records: `cat $fname | wc -l`
-cat $fname) | less
-
+		#
+		#	Now, we append this information to the text file names.dat
+		#       separating the six items of data with ":"
+		#
+		echo $firstname:$surname:$address:$city:$state:$zip >> $fname
+		echo
+		echo A new record has been created. 
+		echo Thank you for the input.
+		;;
+	2)
+		#
+		#	The contents and count of the number of records 
+		#	of the file will be displayed using the "less" page 
+		#	program.
+		#
+		echo 
+		(echo "The database contains `cat $fname | wc -l` record(s)."
+		echo Here are the contents of the file $fname.
+		echo
+		cat $fname) | less
+		;;
+	3)
+		echo Not yet implemented.
+		;;	
+	4)
+		echo Not yet implemented.
+		;;
+	q)
+		#
+		#	Program is terminated within a given time of 2 seconds.
+		#
+		echo Exiting.. Hope to see you soon!
+		sleep 2
+		;;
+	*)
+		echo Your input is invalid.
+		;;
+esac
